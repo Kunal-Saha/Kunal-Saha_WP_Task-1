aad65846ee71b76ee68c0e4500fdd30693596143
Kunal <kunal.saha@wisdmlabs.com>
2023-03-25 01:51:56 +0530

    Adding the require header functions and html in the header file

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <?php wp_head() ?>
+        <title>Html-Assignment~Kunal</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <h3 class="Nav__header">Header Logo</h3>
+                <ul class="Nav__menu">
+                    <a href=""> <li class="Nav__items">Home</li> </a>
+                    <a href=""> <li class="Nav__items">About</li> </a>
+                    <a href=""> <li class="Nav__items">Contact</li> </a>
+                </ul>
+            </nav>
+        </header>
